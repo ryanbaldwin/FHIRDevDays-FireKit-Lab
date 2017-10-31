972ed7ebdc47ba89fa7a7022d36a60979e772603
Ryan Baldwin <ryanbaldwin@gmail.com>
2017-10-31 10:47:01 -0400

    tweaked comments for readibility

--- FHIRPatients/PatientModel.swift
+++ FHIRPatients/PatientModel.swift
@@ -154,21 +154,24 @@ class PatientModel {
             return
         }
         
-        // FHIRDevDays Lab TODO - 1:
-        // 1.   Convert the in-memory model to a FireKit Patient persist that patient locally.
-        //      Do not assign an `id` to the Patient object, as we will have the server assign one for us.
-        //      In subsequent exercises we will use the existence of the `id` field to determine if the patient has
-        //      been uploaded to or not.
-        //      (hint: after a successul save, set `patientToEdit` to the patient created here)
-        //
-        // 2.   Update this `save` function to account the situation where this PatientModel was initialized
-        //      with a Patient to Edit.
-        //
-        // Hints:
-        //  - for the Photo: You can get a JPEG representation of the `userImage` using `UIImageJPEGRepresentation`
-        //  - You can create a Base64Binary instance (`Attachment.data`) using `UIImageJPEGRepresentation.base64EncodedString(options:)`
-        //  - Be careful when saving the `telecom` array! For this tutorial it's easier if you add copies of them
-        //    to the `Patient.telecom` list instead of adding them directly. You can use a combination of `flatMap` and `copy()`
+    /*******
+         FHIRDevDays Lab TODO - 1:
+         1.   Convert the in-memory model to a FireKit Patient persist that patient locally.
+         Do not assign an `id` to the Patient object, as we will have the server assign one for us.
+         In subsequent exercises we will use the existence of the `id` field to determine if the patient has
+         been uploaded to or not.
+         (hint: after a successul save, set `patientToEdit` to the patient created here)
+         
+         2.   Update this `save` function to account the situation where this PatientModel was initialized
+         with a Patient to Edit.
+         
+         Hints:
+         -  for the Photo: You can get a JPEG representation of the `userImage` using `UIImageJPEGRepresentation`
+         -  You can create a Base64Binary instance (`Attachment.data`) using `UIImageJPEGRepresentation.base64EncodedString(options:)`
+         -  Be careful when saving the `telecom` array! For this tutorial it's easier if you add copies of them
+            to the `Patient.telecom` list instead of adding them directly. You can use a combination of `flatMap` and `copy()`
+         
+     *******/
     }
     
     /// Uploads this patient to the server and
@@ -192,17 +195,19 @@ class PatientModel {
                                         error: nil)
         }
         
-        // FHIRDevDays Lab TODO - 2:
-        // 1.   In the `FHIRPatients/http/HttpPatient.swift`, create 2 Restivus requests for uploading a Patient:
-        //      1 for POST'ing a new Patient to the server, and one for PUTing an existing patient to the server.
-        //      Each request should conform to the `Encodable`, `Authenticating, and the respective Restable protocol:
-        //      `Postable` for POST, and `Puttable` for PUT. Each request should have a `ResponseType` of `Patient`.
-        //
-        // 2.   Here, submit the appropriate request to add a new patient, or update the patient if it already exists
-        //      on the server. We will use the presence of an `id` on the patient object to indicate if its been
-        //      uploaded to the server or not. Upon a successful response, update the local patient to match the patient
-        //      returned in the Restable response.
-        //      (hint: using the `Patient.populate(from:)` function on a `Patient` instance makes this easy)
+    /*******
+        FHIRDevDays Lab TODO - 2:
+        1.   In the `FHIRPatients/http/HttpPatient.swift`, create 2 Restivus requests for uploading a Patient:
+             1 for POST'ing a new Patient to the server, and one for PUTing an existing patient to the server.
+             Each request should conform to the `Encodable`, `Authenticating, and the respective Restable protocol:
+             `Postable` for POST, and `Puttable` for PUT. Each request should have a `ResponseType` of `Patient`.
+        
+        2.   Here, submit the appropriate request to add a new patient, or update the patient if it already exists
+             on the server. We will use the presence of an `id` on the patient object to indicate if its been
+             uploaded to the server or not. Upon a successful response, update the local patient to match the patient
+             returned in the Restable response.
+             (hint: using the `Patient.populate(from:)` function on a `Patient` instance makes this easy)
+    *******/
     }
     
     /// Downloads this patient from the server and updates it locally.
@@ -227,17 +232,18 @@ class PatientModel {
                 message: "Cannot download the patient at this time. Does the patient have a proper FHIR Reference?",
                 error: nil)
         }
-        
-        // FHIRDevDays Lab TODO - 3:
-        // 1.   In the `FHIRPatients/http/HttpPatient.swift` file, create a third `Restivus` request for downloading
-        //      a specific patient (based on this instance's patient's `id`).
-        //
-        // 2.   Using the download request created in step `1.`, download the patient matching the `id` of this
-        //      PatientModel's patient. Update the local patient with the one from the server.
-        //
-        // Hints:
-        //  - Don't forget to set `self.patientToEdit` to the saved copy
-        //  - using `upsert(_:)` makes saving this a breeze.
+    /*******
+         FHIRDevDays Lab TODO - 3:
+         1.   In the `FHIRPatients/http/HttpPatient.swift` file, create a third `Restivus` request for downloading
+         a specific patient (based on this instance's patient's `id`).
+         
+         2.   Using the download request created in step `1.`, download the patient matching the `id` of this
+         PatientModel's patient. Update the local patient with the one from the server.
+         
+         Hints:
+         - Don't forget to set `self.patientToEdit` to the saved copy
+         - using `upsert(_:)` makes saving this a breeze.
+     *******/
     }
 }
 
